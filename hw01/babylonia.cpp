a3c6655a13a517e9dd9fae7d7d5aa55055b4b7f0
Zephan Spencer <number45zephan@gmail.com>
2020-01-21 14:15:19 -0500

    Almost finished Bablyon

--- hw01/babylonia.cpp
+++ hw01/babylonia.cpp
@@ -20,43 +20,59 @@
 
 using namespace std;
 
-//Funtion prototype
-double takeInput();
-double squareRoot(double userInput);
-double calculatePctErr(double approximation, double userInput);
-void outputValues(double approximation, double userInput, double pctErr);
+//Function prototypes
+void takeInput(double &input);
+double squareRoot(double input);
+double calculatePctErr(double approximation, double input);
+void outputValues(double approximation, double input, double pctErr);
 
 int main(void){
-	double input, guess, pcErr;
-	input = takeInput();
-	guess = squareRoot(input);
-	pcErr = calculatePctErr(guess, input);
-	outputValues(guess, input, pcErr);
-	
+	double input, approximation, pcErr;
+	takeInput(input);
+
+	approximation = squareRoot(input);
+
+	pcErr = calculatePctErr(approximation, input);
+	outputValues(approximation, input, pcErr);
+
 	return 0;
 }
 
 //Function that takes user input to calculate an approximate square root
-double takeInput(){
-	//Your code goes here
+void takeInput(double &input){
+	cout << "Please enter your number: ";
+	cin >> input;
+	cout << endl;
 }
 
 /*Function that calculates an approximation of a square root using the 
 Babylonian method
 */
-double squareRoot(double userInput){
-	//Your code goes here
+double squareRoot(double input){
+	double guess = 2.0;
+	double r;
+
+	for(int i = 0; i<5; i++){
+		r = input/guess;
+		guess = (guess+r)/2.0;
+	}
+
+	return guess;
 }
 
 //Function that calculates the percent error in the approximation
-double calculatePctErr(double approximation, double userInput){
-	//Your code goes here
+double calculatePctErr(double approximation, double input){
+	double actual = sqrt(input);
+	double pctErr = abs(approximation-actual)*100.0/actual;
+	
+	return pctErr;
 }
 
 /*Function that outputs the approximation and actual square root
 values and percent error value.
 */
-void outputValues(double approximation, double userInput, double pctErr){
-	//Your code goes here
+void outputValues(double approximation, double input, double pctErr){
+	printf ("%.2f\n", approximation);
+	printf ("%.2f\n", pctErr);
 }
 
